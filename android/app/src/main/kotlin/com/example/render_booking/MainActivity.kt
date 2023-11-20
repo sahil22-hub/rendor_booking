fb307eba68f205cc989ee9c43ecf10f17d95096c
sahil <sahilbhetwal22@gmail.com>
2023-11-20 10:45:21 +0545

    initial push

---
+++ android/app/src/main/kotlin/com/example/render_booking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.render_booking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/config/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromRGBO(255, 106, 0, 1);
+  static const secondaryColor = Color.fromRGBO(26, 44, 72, 1);
+}
